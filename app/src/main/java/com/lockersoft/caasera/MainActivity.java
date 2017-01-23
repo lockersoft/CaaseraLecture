110d04f9d5f43406061fec1dc9f4eb583a9126b4
Dave Jones <Dave.Jones@scc.spokane.edu>
2017-01-23 10:26:44 -0800

    added "Keep me logged in" checkbox

--- app/src/main/java/com/lockersoft/caasera/MainActivity.java
+++ app/src/main/java/com/lockersoft/caasera/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,17 +12,21 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText edtLoginName;
+    CheckBox chbLoggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtLoginName = (EditText) findViewById(R.id.edtLoginName);
+        chbLoggedIn = (CheckBox) findViewById(R.id.chbStayLoggedIn);
     }
 
     public void btnLoginOnClick(View v) {
         Log.i("CLICK", "Login button was clicked.");
         Log.i("CLICK", edtLoginName.getText().toString());
-        Toast.makeText(this, "You successfully Logged in: " + edtLoginName.getText().toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "You successfully Logged in: " +
+                edtLoginName.getText().toString() + " " +
+                chbLoggedIn.isChecked(), Toast.LENGTH_LONG).show();
     }
 }

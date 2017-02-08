b00e93bd1c7bbc0ab9cc4161c660eee66854039b
Dave Jones <Dave.Jones@scc.spokane.edu>
2017-02-08 13:18:56 -0800

    interim commit for transport

--- app/src/main/java/com/lockersoft/caasera/Landing2.java
+++ app/src/main/java/com/lockersoft/caasera/Landing2.java
@@ -2,12 +2,38 @@ package com.lockersoft.caasera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Landing2 extends BaseActivity {
+    final TextView mTextView = (TextView) findViewById(R.id.text);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing2);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 }
+
+
+

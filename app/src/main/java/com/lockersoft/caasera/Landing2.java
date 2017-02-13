cb6afa879996980b4cc7e8af99daeb2a09e88fb0
Dave Jones <Dave.Jones@scc.spokane.edu>
2017-02-13 10:21:52 -0800

    Added volley lib and test code

--- app/src/main/java/com/lockersoft/caasera/Landing2.java
+++ app/src/main/java/com/lockersoft/caasera/Landing2.java
@@ -4,34 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class Landing2 extends BaseActivity {
-    final TextView mTextView = (TextView) findViewById(R.id.text);
+    TextView txtWebContent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing2);
+        txtWebContent = (TextView) findViewById(R.id.txtWebContent);
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        String url = "https://caasera.azurewebsites.net/api/1.0/student";
+        // Request a string from the URL
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,500));
+                        txtWebContent.setText("Response: " + response.substring(0, 500));
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        txtWebContent.setText("That didn't work!!!");
+                    }
+                }
 
+        );
+        queue.add( stringRequest );
     }
 }
 
